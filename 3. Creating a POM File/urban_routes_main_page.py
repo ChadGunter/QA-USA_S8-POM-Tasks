ad499161be37bf50dfd3f429a242e36d68b0ed44
ChadGunter <whatthechad@gmail.com>
2025-11-07 14:18:19 -0800

    urban_routes_main_page_filled_out

--- 3. Creating a POM File/urban_routes_main_page.py
+++ 3. Creating a POM File/urban_routes_main_page.py
@@ -23,12 +23,12 @@ class UrbanRoutesPage:
 
     def click_custom_option(self):
         # Click the "Custom" option
-        ...
+        self.driver.find_element(*self.CUSTOM_OPTION_LOCATOR).click()
 
     def click_bike_icon(self):
         # Click the "Bike" icon
-        ...
+        self.driver.find_element(*self.BIKE_ICON_LOCATOR).click()
 
     def get_bike_text(self):
         # Return the "Bike" text
-        ...
+        self.driver.find_element(*self.BIKE_TEXT_LOCATOR).text
